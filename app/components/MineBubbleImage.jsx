0289442ca607b340a08b2f3ea23f2a9b55d8ff62
Amor Ali <alibenamor30@gmail.com>
2021-01-09 19:32:36 +0100

    import text component fix

--- app/components/MineBubbleImage.jsx
+++ app/components/MineBubbleImage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import {View, StyleSheet, Image, Text} from 'react-native';
 import {COLORS} from './__styleVars';
 export default function MineBubbleImage({message}) {
   const styles = StyleSheet.create({
